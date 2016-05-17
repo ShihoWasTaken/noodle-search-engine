7d6c03020bc5b5e28a8e474e1f85a66afe7ae5fe
shiho <kenny.guiougou@gmail.com>
2016-05-17 15:23:21 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+	<head>		
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Search engine</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1>Noodle</h1>
+	                <form role="form">
+	                    <div class="input-group">
+	                        <div class="input-group-btn">
+	                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
+	                            <ul class="dropdown-menu">
+	                                <li><a href="#">Action</a></li>
+	                                <li><a href="#">Another action</a></li>
+	                                <li><a href="#">Something else here</a></li>
+	                                <li role="separator" class="divider"></li>
+	                                <li><a href="#">Separated link</a></li>
+	                            </ul>
+	                        </div>
+	                        <input type="text" class="form-control" placeholder="Entrez votre recherche">
+	                        <span class="input-group-btn">
+	                            <input class="btn btn-primary" type="submit" value="chercher">
+	                        </span>
+	                    </div>
+	                </form>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
