7417dca25d641054498ce7a76f88d39e7e280b80
shiho <kenny.guiougou@gmail.com>
2016-05-17 22:47:38 +0200

    Refonte accueil et ajout page résultat

--- index.php
+++ index.php
@@ -5,39 +5,48 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Search engine</title>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <title>Noodle | Accueil</title>
+        <!-- JQuery 2 -->
+        <script   src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet' type='text/css'>
+		<link href='css/index.css' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
 		<div class="container">
 			<div class="row">
-				<div class="col-md-12">
-					<h1>Noodle</h1>
-	                <form role="form">
-	                    <div class="input-group">
-	                        <div class="input-group-btn">
-	                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
-	                            <ul class="dropdown-menu">
-	                                <li><a href="#">Action</a></li>
-	                                <li><a href="#">Another action</a></li>
-	                                <li><a href="#">Something else here</a></li>
-	                                <li role="separator" class="divider"></li>
-	                                <li><a href="#">Separated link</a></li>
-	                            </ul>
-	                        </div>
-	                        <input type="text" class="form-control" placeholder="Entrez votre recherche">
-	                        <span class="input-group-btn">
-	                            <input class="btn btn-primary" type="submit" value="chercher">
-	                        </span>
-	                    </div>
-	                </form>
+				<div class="col-md-12 vcenter">
+					<div class="noflex">					
+						<h1>
+							<div class="logo text-center">
+								<span class="blue-letter">N</span><span class="red-letter">o</span><span class="yellow-letter">o</span><span class="blue-letter">d</span><span class="green-letter">l</span><span class="red-letter">e</span>
+							</div>
+						</h1>
+		                <form role="form" method="post" action="search.php">
+		                    <div class="input-group">
+		                        <div class="input-group-btn">
+		                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
+		                            <ul class="dropdown-menu">
+		                                <li><a href="#">Action</a></li>
+		                                <li><a href="#">Another action</a></li>
+		                                <li><a href="#">Something else here</a></li>
+		                                <li role="separator" class="divider"></li>
+		                                <li><a href="#">Separated link</a></li>
+		                            </ul>
+		                        </div>
+		                        <input id="searchbar" type="text" class="form-control" placeholder="Entrez votre recherche">
+		                        <span class="input-group-btn">
+		                            <input class="btn btn-primary" type="submit" value="chercher">
+		                        </span>
+		                    </div>
+		                </form>
+					</div>
 				</div>
 			</div>
 		</div>
