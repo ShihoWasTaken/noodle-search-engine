604d66bb8e493b9fe0eb7cde33e4459192dd8594
shiho <kenny.guiougou@gmail.com>
2016-05-18 23:02:59 +0200

    Algo sans stem

--- indexing.py
+++ indexing.py
@@ -10,6 +10,8 @@
 import xml.etree.ElementTree
 import os
 import sys
+import collections
+import re
 
 #########################################################################################
 #																						#
@@ -23,6 +25,8 @@ import sys
 #																						#
 #########################################################################################
 
+docNoList = collections.OrderedDict()	# La map qui va contenir la liste des docno avec pour chaque clé un stem
+
 #########################################################################################
 #																						#
 #										Fonctions										#
@@ -35,11 +39,22 @@ import sys
 #																						#
 #########################################################################################
 
-# Pour chaque fichier du dossier data on effectuera les traitements qui suivent
-for fichier in os.listdir('data/'):
-	root = xml.etree.ElementTree.parse('data/' + fichier).getroot()
-	for childs in root:
-		for child in childs:
-			if (child.tag.upper() == "TEXT"):
-				print child.text
-				sys.exit(0)
+if __name__ == '__main__':
+	# Pour chaque fichier du dossier data on effectuera les traitements qui suivent
+	for fichier in os.listdir('data/'):
+		root = xml.etree.ElementTree.parse('data/' + fichier).getroot()
+		for childs in root:
+			docno = None
+			for child in childs:
+				if (child.tag.upper() == "DOCNO"):
+					docno = child.text
+				elif (child.tag.upper() == "TEXT"):
+					words = re.split(' |\n', child.text)
+					for word in words:
+						if(docNoList.get(word, None) is None):		# On teste si la clé du dictionnaire est vide
+							print "On crée la clé " + word + " avec le num " + docno
+							docNoList[word] = [docno]					# On crée une liste contenant le docno actuel
+						else:										# Si elle n'est pas vide
+							print "On ajoute le num " + docno + " à docNoList[" + word + "]"
+							docNoList[word].append(docno)			# On ajoute le docno à la liste
+		sys.exit(0) # On termine après le premier fichier
