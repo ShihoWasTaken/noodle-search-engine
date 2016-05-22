51d125ec6a41fa17b52d4552183080fa00d57d52
shiho <kenny.guiougou@gmail.com>
2016-05-22 13:34:25 +0200

    optimisation de l'algo

--- indexing.py
+++ indexing.py
@@ -59,6 +59,26 @@ def getStemOrderedDict():
 def stemmer(word):
 	return PorterStemmer().stem_word(word).decode().encode('utf-8')
 
+def trim(word):
+	word = word.replace(".","")
+	word = word.replace(",","")
+	word = word.replace(";","")
+	word = word.replace("?","")
+	word = word.replace("!","")
+	word = word.replace("/","")
+	word = word.replace("_","")
+	word = word.replace("'","")
+	word = word.replace("`","")
+	word = word.replace("(","")
+	word = word.replace(")","")
+	word = word.replace("[","")
+	word = word.replace("]","")
+	word = word.replace("{","")
+	word = word.replace("}","")
+	word = word.replace(":","")
+	word = word.lower()
+	return word
+
 #########################################################################################
 #																						#
 #									  	   Main			 								#
@@ -83,15 +103,15 @@ if __name__ == '__main__':
 					if child.text is not None:
 						words = re.split(' |\n|\t', child.text)
 						for word in words:
-							word = word.strip(".,_`'")
-							word = word.lower()
+							word = trim(word)
+							stemmedWord = stemmer(word)
 							if word not in stopwords:
-								if(docNoList.get(stemmer(word), None) is None):		# On teste si la clé du dictionnaire est vide
+								if(docNoList.get(stemmedWord, None) is None):		# On teste si la clé du dictionnaire est vide
 									#print "On crée la clé " + stemmer(word) + " avec le num " + docno
-									docNoList[stemmer(word)] = [docno]					# On crée une liste contenant le docno actuel
+									docNoList[stemmedWord] = [docno]					# On crée une liste contenant le docno actuel
 								else:										# Si elle n'est pas vide
 									#print "On ajoute le num " + docno + " à docNoList[" + stemmer(word) + "]"
-									docNoList[stemmer(word)].append(docno)			# On ajoute le docno à la liste
+									docNoList[stemmedWord].append(docno)			# On ajoute le docno à la liste
 	indexedFile = open("indexedStem.txt","w")
 	for stem, listDoc in docNoList.iteritems():
 		indexedFile.write(stem + " | " + " ".join(listDoc) + "\n")
